8f42b64537ba295f0d6a0233b246e46a7e651bc3
Brian Retamar <leo1632@hotmail.com>
2021-07-15 09:29:06 -0300

    Refactor tests helpers

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,6 +1,6 @@
 const supertest = require('supertest')
 const app = require('../app')
-
+const User = require('../models/User')
 const api = supertest(app)
 
 const initialNotes = [
@@ -29,8 +29,14 @@ const getAllContentFromNotes = async () => {
     }
 }
 
+const getUsers = async () => {
+    const usersDB = await User.find()
+    return usersDB.map(user => user.toJSON())
+}
+
 module.exports = {
     initialNotes,
     api,
-    getAllContentFromNotes
+    getAllContentFromNotes,
+    getUsers
 }
