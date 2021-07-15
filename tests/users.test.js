aac1c5b19ef4af1967f76c82e644c3162f010bf0
Brian Retamar <leo1632@hotmail.com>
2021-07-15 17:08:51 -0300

    Created test for 'that username already exists'

--- tests/users.test.js
+++ tests/users.test.js
@@ -37,6 +37,26 @@ describe.only('Creating a new user', () => {
         expect(usernames).toContain(newUser.username)
     })
 
+    test('creation fails with proper status-code and message if username is already taken', async () => {
+        const usersAtStart = await getUsers()
+
+        const newUser = {
+            username: 'miduroot',
+            name: "Miguel",
+            password: 'test'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(409)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.errors.username.message).toContain('`username` to be unique')
+
+        const usersAtEnd = await getUsers()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
 
     afterAll(() => {
         mongoose.connection.close()
