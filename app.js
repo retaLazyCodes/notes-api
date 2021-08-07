514c7b8ecc95057e2938a6730ca62ac5c6c73b08
Brian Retamar <leo1632@hotmail.com>
2021-08-07 20:01:50 -0300

    Refactor app

--- app.js
+++ app.js
@@ -1,45 +1,22 @@
 require('dotenv').config()
-require('./mongo')
+require('./config/dbConection')
 
 const express = require('express')
 const cors = require('cors')
 const logger = require("morgan");
-const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const notFound = require('./middleware/notFound.js')
 const handleErrors = require('./middleware/handleErrors.js')
-const notesRouter = require('./routes/notes.routes')
-const usersRouter = require('./routes/users.routes')
-const loginRouter = require('./routes/login.routes')
 
 // initializate app
 const app = express()
 
-// Extended: https://swagger.io/specification/#infoObject
-const swaggerOptions = {
-    swaggerDefinition: {
-        swagger: "2.0",
-        info: {
-            version: "version 0.1.0",
-            title: "Notes API",
-            description: "Notes API Information",
-            contact: {
-                name: "Brian Retamar"
-            },
-            servers: [{
-                url: `${process.env.PORT}`,
-            }],
-        }
-    },
-    apis: ['./routes/*.js']
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/",
+const swaggerDocs = require('./config/swagger')
+// setup swagger
+app.use("/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocs)
-);
-
+)
 
 // middlewares
 app.use(logger("dev"));
@@ -48,9 +25,10 @@ app.use(express.json())
 app.use('/images', express.static('images'))
 
 // routes
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+
+app.use('/api/notes', require('./routes/notes.routes'))
+app.use('/api/users', require('./routes/users.routes'))
+app.use('/api/login', require('./routes/login.routes'))
 
 // custom middlewares
 app.use(notFound)
